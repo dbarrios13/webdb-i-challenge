f1b71de488b38a9a65d268d0b9bc94a81758eead
dbarrios13 <barriosadavid@gmail.com>
2019-06-10 15:17:54 -0700

    database created working on CRUD

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require('express');
 
+const db = require('./data/accounts-model')
+
 const server = express();
 
-// your code here
+server.use(express.json())
+
+server.get('/', async(req, res) => {
+    try {
+        const budgets = await db.find()
+        res.status(200).json(budgets)
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error retrieving budget data'
+        })
+    }
+})
+
+server.post('/', async (req, res) => {
+    try {
+        const newBudget = await db.add(req.body)
+        res.status(201).json(newBudget)
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error adding new budget'
+        })
+    }
+})
 
 module.exports = server;
